74e9d68bc2a565028f7cf2d977815c4c488f6cd4
[samia <32800475+samiabari@users.noreply.github.com>
2024-07-19 01:19:09 +0800

    stack

--- StackDataStructure/StackDataStructure/Program.cs
+++ StackDataStructure/StackDataStructure/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int size = int.Parse(Console.ReadLine());
+            Stack<int> s = new Stack<int>(size);
+            s.Push(1);
+            s.Push(2);
+            s.Push(3);
+            s.Push(4);
+            s.Print();
+            s.Peek();
+            s.Pop();
+            s.Pop();
+            s.Print();
         }
     }
 }
---
+++ StackDataStructure/StackDataStructure/Stack.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackDataStructure
+{
+    public class Stack<T>
+    {
+        static T[] elements;
+        static int top;
+        static int max;
+        public Stack(int size) { 
+            elements=new T[size];
+            top = -1;
+            max = size;
+        }
+
+        public  void Push(T item)
+        {
+            if (top == max - 1)
+            {
+                Console.WriteLine("over flow");
+                return;
+            }
+            top++;
+            elements[top]=item;
+        }
+
+        public  T Pop()
+        {
+            if (top == - 1)
+            {
+                Console.WriteLine("underflow");
+                return default(T);
+            }
+            T itemReturn = elements[top];
+            --top;
+            return itemReturn;
+        }
+        public  T Peek()
+        {
+            if (top == -1)
+            {
+                Console.WriteLine("empty");
+                return default(T);
+            }
+            
+            return elements[top];
+        }
+
+        public void Print()
+        {
+            if (top == -1)
+            {
+                Console.WriteLine("empty");
+               
+            }
+
+            for (int i = top; i >= 0; i--)
+            {
+                Console.WriteLine(elements[i]);
+            }
+        }
+    }
+}
